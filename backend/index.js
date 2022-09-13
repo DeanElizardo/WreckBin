70b510d753e0ac9a5bd0a1224aa45e255d40fa71
Dean Elizardo <del@delizardo.com>
2022-09-13 21:02:00 +0000

    began adding routes for user portal and request capture api

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { viewRouter } from './routes/view.js';
+import { recordRouter } from './routes/record.js';
+import * from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use('/bins', viewRouter);
+
+app.use('/record', recordRouter);
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.send(process.env.RESOURCE_NOT_FOUND);
+}
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.send(process.env.SERVER_ERROR);
+});
+
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log("Server is up and listening on port", process.env.PORT);
+});
---
+++ backend/routes/view.js
@@ -0,0 +1,13 @@
+/**
+ * This file contains all of the routes for handling 
+ * user interactions with the front end
+ */
+import express from 'express';
+
+export const router = express.Router();
+
+router.get('/', (req, res) => {});
+
+router.get('/:id', (req, res) => {});
+
+router.post();
