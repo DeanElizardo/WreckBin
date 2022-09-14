1068cf5e5bbada2baeaf8d0d67e54d889fa921c6
Joey <josephdguillaume@gmail.com>
2022-09-14 08:43:37 -0400

    Added initial SQL and MongoDB Schemas.

---
+++ backend/db.js
@@ -0,0 +1,23 @@
+/*
+
+TODO: Implement in Node-Postgres
+
+CREATE TABLE Bins {
+  id INT PRIMARY KEY,
+  ipAddress varchar(15) NOT NULL,
+  url varchar(255) NOT NULL,
+  createdOn timestamp NOT NULL,
+  lastAccessed timestamp NOT NULL,
+  deleted boolean DEFAULT false NOT NULL,
+}
+
+CREATE TABLE Requests {
+  id INT PRIMARY KEY,
+  binId INT FOREIGN KEY REFERENCES Bins(id)
+  requestType varchar(6)
+  createdOn timestamp NOT NULL,
+  incomingIp varchar(15) NOT NULL,
+  uniqueDocId varchar(255),
+}
+
+*/
