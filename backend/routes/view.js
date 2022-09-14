f5930abae5d4a97e4d8ef2d82582375ec89327ec
Dean Elizardo <del@delizardo.com>
2022-09-14 10:42:46 -0500

    comments for routes

--- backend/routes/view.js
+++ backend/routes/view.js
@@ -3,11 +3,21 @@
  * user interactions with the front end
  */
 import express from 'express';
-
+import pg from 'pg';
 export const router = express.Router();
 
+/*
+ * home page
+ */
 router.get('/', (req, res) => {});
 
+/* 
+ * get a reference for all records 
+ * with this bin id
+ */
 router.get('/:id', (req, res) => {});
 
-router.post();
+/*
+ * generate a new bin
+ */
+router.post('/', (req, res) => );
