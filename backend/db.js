9e78edf7c8c56759b171f60c1b219584683f50da
Joey <josephdguillaume@gmail.com>
2022-09-14 11:26:46 -0400

    Added Query method for node-postgres.

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
+
 /*
 
 TODO: Implement in Node-Postgres
