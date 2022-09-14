9af35fadded0f2cafd96acbfcad42e03c8793f6a
Joey <josephdguillaume@gmail.com>
2022-09-14 18:57:10 -0400

    Restructured views for View Router.

--- backend/routes/view.js
+++ backend/routes/view.js
@@ -8,15 +8,27 @@ const { generateID } = require('../services/generateBinID.js');
 
 const router = express.Router();
 
-/* view homepage */
-router.get('/', (req, res) => {});
+/* view homepage with all our bins */
+router.get('/', (req, res) => {
+  // If user id exists in local storage, retrieve bins if they exist
+  // If not, generate user id and store in local storage, bins dont exist
+});
 
-/* get all records associated with bin id */
-router.get('/:userID', (req, res) => {});
+/* view individual bin */
+router.get('/:binid', (req, res) => {});
+
+/* get all records associated with user id */
+// router.get('/:userID', (req, res) => {});
+
+/* view individual request */
+router.get('/:requestId', (req, res) => {});
 
 /* generate a new bin */
 router.post('/new', (req, res) => {
+  // Create a new bin
   const newBinID = generateID();
+
+  // Redirect to the path of '/'view '
 });
 
 module.exports = router;
